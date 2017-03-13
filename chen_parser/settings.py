1ec4935d5c85691b02e966526684cb7bffeea555
Thuong-Hai Pham <phamthuonghai@gmail.com>
2017-03-13 15:49:12 +0100

    [chen] Remove labels from features, 0->680 successes on dev

--- chen_parser/settings.py
+++ chen_parser/settings.py
@@ -2,7 +2,8 @@ from common.conllu import *
 
 TEMPLATE_TO_CONLLU = {'w': FORM, 't': UPOSTAG, 'l': DEPREL}
 VOCAB_LIMIT = {'w': 10000, 't': -1, 'l': -1}
-DATA_TYPES = ['w', 't', 'l']
+# DATA_TYPES = ['w', 't', 'l']
+DATA_TYPES = ['w', 't']
 EMBEDDING_SIZE = 50
 HIDDEN_LAYER_SIZE = 200
 BATCH_SIZE = 200
