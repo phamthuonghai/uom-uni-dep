4acae8f334fb51d51e48951f2459d023103a6c59
Thuong-Hai Pham <phamthuonghai@gmail.com>
2017-03-16 00:23:42 +0100

    Run all countries with dev set

---
+++ job_countries_dev.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+#
+# Select Q
+#$ -q albert.q
+#
+# Your job name
+#$ -N chen_parser
+#
+# Use current working directory
+#$ -cwd
+#
+# Join stdout and stderr
+#$ -j y
+#$ -o countries-dev.out
+#
+# Run job through bash shell
+#$ -S /bin/bash
+#
+export PYTHONPATH=$PYTHONPATH:$(pwd)
+export THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32
+export PYTHON=$(pwd)/venv/bin/python
+
+TEMPLATE_FILE=./config/chen-nolabel-nochild.template
+
+files="./data/ud-treebanks-conll2017/*/*-ud-dev.conllu"
+regex="UD_([A-Za-z\-]+)\/([a-z_]+)-ud-dev.conllu"
+
+for f in $files
+do
+    if [[ $f =~ $regex ]]
+    then
+        echo "=========================================================================="
+        echo "                          LANGUAGE: ${BASH_REMATCH[1]} ${BASH_REMATCH[2]}"
+        echo "=========================================================================="
+
+        TRAIN_INPUT="./data/ud-treebanks-conll2017/UD_${BASH_REMATCH[1]}/${BASH_REMATCH[2]}-ud-train.conllu"
+        FEATURE_FILE="./models/${BASH_REMATCH[2]}-ud-train-nolabel-nochild-ft.pkl"
+        MODEL_PREFIX="./models/${BASH_REMATCH[2]}-ud-train-nolabel-nochild"
+        PARSE_INPUT="./data/ud-treebanks-conll2017/UD_${BASH_REMATCH[1]}/${BASH_REMATCH[2]}-ud-dev.conllu"
+        PARSE_OUTPUT="./models/${BASH_REMATCH[2]}-ud-res-nolabel-nochild.conllu"
+
+        echo "========================= FEATURES EXTRACTION ${BASH_REMATCH[2]} ========================="
+        $PYTHON -m chen_parser.feature -t $TEMPLATE_FILE $TRAIN_INPUT $FEATURE_FILE
+        echo "=========================== TRAINING ORACLE ${BASH_REMATCH[2]} ==========================="
+        $PYTHON -m chen_parser.oracle $FEATURE_FILE $MODEL_PREFIX
+        echo "========================== PARSING TEST DATA ${BASH_REMATCH[2]} =========================="
+        $PYTHON -m chen_parser.parser -t $TEMPLATE_FILE $PARSE_INPUT $MODEL_PREFIX $PARSE_OUTPUT
+        echo "=========================== TESTING RESULTS ${BASH_REMATCH[2]} ==========================="
+        $PYTHON ./conll/evaluation_script/conll17_ud_eval.py --verbose $PARSE_INPUT $PARSE_OUTPUT
+    fi
+done
